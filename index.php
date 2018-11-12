da72b833bcc4e7b692fd8f668a07d49826be3ff1
Léopold Mebazaa <lemebfr@gmail.com>
2018-11-12 10:51:43 -0500

    Put placeholder database test

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <?php
 $conn = pg_connect(getenv("DATABASE_URL"));
+$query = 'SELECT * FROM events';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+
 ?>
 
 Hello world
