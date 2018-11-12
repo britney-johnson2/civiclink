fced12cbf59ceb0c187b2d7860db85e9fef4b437
Léopold Mebazaa <lemeb@github.com>
2018-11-12 16:37:07 -0500

    Why doesn't it work? 🤔 - 18 – Revert to pg_query(). Yes, it sucks.

--- index.php
+++ index.php
@@ -85,13 +85,12 @@ echo "</table>\n";
 pg_free_result($result);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
-  // $res = pg_query($conn, "
-  //   INSERT INTO events 
-  //   (name, begintime, endtime, location, price, description)
-  //   VALUES ('".$_POST['name']."','".$_POST['begin']."',
-  //           '".$_POST['end']."','".$_POST['location']."',
-  //           '".$_POST['price']."','".$_POST['description']."');");
-  $res = pg_insert($conn, "events", $_POST);
+  $res = pg_query($conn, "
+    INSERT INTO events 
+    (name, begintime, endtime, location, price, description)
+    VALUES ('".$_POST['name']."','".$_POST['begin']."',
+            '".$_POST['end']."','".$_POST['location']."',
+            '".$_POST['price']."','".$_POST['description']."');");
   echo "Error Last " . pg_last_error($conn);
   print_r($_POST);
     if ($res) {
